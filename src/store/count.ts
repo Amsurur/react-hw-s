fc8c952248c48be19410e1d61c9265a8f83f0857
Muhammadsurur Abdulloev <abdulloevmuhammadsurur@gmail.com>
2026-06-08 16:09:39 +0500

    finishing hw

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,7 +1,8 @@
 import { create } from 'zustand'
 
 export const useBear = create((set,get) => ({
-    data:[{id:1,name:"Ibrohim"}],
+  data:[{id:1,name:"Ibrohim"}],
+  data2:[{id:1,name:"Ibrohim"}],
     deleteData: () => set((state) => ({ count1: state.count1 + 1 })),
   
   }))
